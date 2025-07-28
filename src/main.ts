203f781df2f70b56867b8fa16c769fa5cbe98ee5
Andre Oliveira <andre.pbo@gmail.com>
2025-07-28 20:30:48 -0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('FII Monitor API')
+    .setDescription(
+      'API para monitoramento de Fundos Imobiliários (FIIs), incluindo cotações e indicadores.',
+    )
+    .setVersion('1.0')
+    .addTag('fiis')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(3000);
 }
 bootstrap();
